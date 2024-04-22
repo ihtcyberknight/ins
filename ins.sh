152c1d7f199c8038315a6a70e477fccbf8bd9ea5
ihtcyberknight <43289059+ihtcyberknight@users.noreply.github.com>
2024-04-23 02:11:50 +0700

    Create ins.sh

---
+++ ins.sh
@@ -0,0 +1,13 @@
+pkg update && upgrade
+termux-setup-storage
+pkg install python2
+pkg install git
+pip install bs4
+pip install rich
+pip install requests
+pip install mechanize
+pip install futures
+pkg install git
+pkg install python
+pkg install python2
+pip install stdiomask
